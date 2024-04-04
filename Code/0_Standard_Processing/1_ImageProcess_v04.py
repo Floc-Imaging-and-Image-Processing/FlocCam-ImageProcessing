5da2c745a8bb5dbd3e37217777e52a14ff434e6e
Kyle Strom <strom@vt.edu>
2024-04-04 14:38:55 -0400

    Update 1_ImageProcess_v04.py

--- Code/0_Standard_Processing/1_ImageProcess_v04.py
+++ Code/0_Standard_Processing/1_ImageProcess_v04.py
@@ -10,8 +10,8 @@ ts = 0 # enter 1 if you want PSD time series output. Enter zero for a single PSD
 dir_size = 60  # number of images to include per distribution (will assume # per minute), ignored if ts = 0
 image_type = '.Bmp' # enter the file extention for your images
 
-IJcode = 'ImageJ-macros/ImageJ_code_diff_v02.txt'
-# IJcode = "ImageJ-macros/ImageJ_code_diff_v02_suppressoutput.txt"  
+# IJcode = 'ImageJ-macros/ImageJ_code_diff_v02.txt'
+IJcode = "ImageJ-macros/ImageJ_code_diff_v02_suppressoutput.txt"  
 # IJcode = "ImageJ-macros/ImageJ_code_diff_watershed_v02.txt"
 
 rmsrc = 0 # remove source images variable. enter 0 for "no" and "1" for yes
